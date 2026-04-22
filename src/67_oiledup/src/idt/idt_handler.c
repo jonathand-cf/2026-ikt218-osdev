d143d6bfa766f0f4f214d7509f3e5a4f46f594d3
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-22 19:28:44 +0200

    Added interrupt handler function with vector switch cases

---
+++ src/67_oiledup/src/idt/idt_handler.c
@@ -0,0 +1,11 @@
+void interrupt_handler(int vector)
+{
+        switch (vector) {
+        case 0:
+            break;
+        case 8:
+            break;
+        case 14:
+            break;
+    }
+}
