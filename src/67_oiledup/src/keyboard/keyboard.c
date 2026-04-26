5469a8ca1f029a04f9fa02ec0ba6a0722d778cab
Emil Andreas Malz <emilam@uia.no>
2026-04-26 12:09:43 +0200

    Semi done keyboard FOR TESTING

---
+++ src/67_oiledup/src/keyboard/keyboard.c
@@ -0,0 +1,31 @@
+#include "keyboard.h"
+#include "../../include/libc/stdio.h"
+
+static const char scancode_to_ascii[] = {
+    0,   0,  '1', '2', '3', '4', '5', '6',
+   '7', '8', '9', '0', '-', '=',  0,   0,
+   'q', 'w', 'e', 'r', 't', 'y', 'u', 'i',
+   'o', 'p', '[', ']',  0,   0,  'a', 's',
+   'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+   '\'','`',  0,  '\\','z', 'x', 'c', 'v',
+   'b', 'n', 'm', ',', '.', '/',  0,   0,
+    0,  ' '
+};
+
+void keyboard_handler(void) {
+    unsigned char scancode;
+
+    scancode = inb(0x60);
+    
+    if (scancode & 0x80) {
+        return;
+    }
+
+    char c = scancode_to_ascii[scancode];
+
+    if (c == 0) {
+        return;
+    }
+    
+    putchar(c);
+}
