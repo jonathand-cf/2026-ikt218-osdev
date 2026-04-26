fd0163f2647aad9d2d78c8a9ed5b67f7cab0b3b1
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-26 10:49:10 +0200

    Refactored ISR wrapper assembly code for clarity and consistency

--- src/67_oiledup/src/idt/isr_wrapper.asm
+++ src/67_oiledup/src/idt/isr_wrapper.asm
@@ -1,22 +1,27 @@
-.globl   isr_wrapper
-.align   4
+global isr0
+global isr8
+global isr14
+
+extern interrupt_handler
+
+section .text
 
 isr0:
-    push 0
+    push dword 0
     jmp isr_wrapper
 
 isr8:
-    push 8
+    push dword 8
     jmp isr_wrapper
 
 isr14:
-    push 14
+    push dword 14
     jmp isr_wrapper
 
 isr_wrapper:
     pushad
-    cld    ; C code following the sysV ABI requires DF to be clear on function entry
+    cld
     call interrupt_handler
     popad
-    add esp, 4   ; remove interrupt number from stack
+    add esp, 4
     iret
